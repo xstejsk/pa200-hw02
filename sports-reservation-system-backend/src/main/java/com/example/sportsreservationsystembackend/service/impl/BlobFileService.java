d3b745a6dd36e39b1f23198d877bcad8d53757ab
xstejsk <stejskalrad@gmail.com>
2025-06-11 22:44:13 +0200

    add managed identity to BE for storage account

--- sports-reservation-system-backend/src/main/java/com/example/sportsreservationsystembackend/service/impl/BlobFileService.java
+++ sports-reservation-system-backend/src/main/java/com/example/sportsreservationsystembackend/service/impl/BlobFileService.java
@@ -41,8 +41,11 @@ public class BlobFileService {
     @Value("${azure.storage.container-name}")
     private String azureStorageContainerName;
 
+    @Value("${azure.storage.user-assigned-client-id}")
+    private String userAssignedClientId;
+
     private BlobContainerClient getBlobContainerClient() {
-        DefaultAzureCredential credential = new DefaultAzureCredentialBuilder().build();
+        DefaultAzureCredential credential = new DefaultAzureCredentialBuilder().managedIdentityClientId(userAssignedClientId).build();
 
         BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
             .endpoint(azureStorageUrl)
